744c061b5a714efcf8e6645e3e54bbd78bc587bb
patrickvrb <patrickvrb@gmail.com>
2019-11-09 19:07:30 -0300

    api with successful requests

--- backend/restAPI/core/views.py
+++ backend/restAPI/core/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.http import JsonResponse
 import requests
 
-def next_launch(request):
-    r = requests.get('https://api.spacexdata.com/v3/launches/next' )
-    launches = r.json()
-    # books_list = {'books':books['results']}
-    return render(request,'index.html',{
-        'name': launches['mission_name'],
-        'year': launches['launch_year'],
-    })
+def queries(request):
+
+    launches    = {}
+    next_launch = {}
+    last_launch = {}
+    data = []
+
+    r = requests.get('https://api.spacexdata.com/v3/launches/next')
+    launches["next_launch"] = r.json()
+    next_launch["mission_name"] = launches["next_launch"]["mission_name"]
+    next_launch["launch_year"] = launches["next_launch"]["launch_year"]
+    data.append(next_launch)
+
+    r = requests.get('https://api.spacexdata.com/v3/launches/latest')
+    launches["last_launch"] = r.json()
+    last_launch["mission_name"] = launches["last_launch"]["mission_name"]
+    last_launch["launch_year"] = launches["last_launch"]["launch_year"]
+    data.append(last_launch)
+
+
+    return JsonResponse(data, safe = False)
--- backend/restAPI/restAPI/urls.py
+++ backend/restAPI/restAPI/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import next_launch
+from core.views import queries
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', next_launch),
+    path('', queries),
 ]
