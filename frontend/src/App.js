4accd62cc7354142cc6486c21428fd2440a36103
patrickvrb <patrickvrb@gmail.com>
2019-11-10 22:09:54 -0300

    react frontend with 'Launches' component

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Launches from './components/launches'
+
+class App extends Component{
+
+  state = {
+    launches: []
+  }
+  componentDidMount() {
+    fetch('http://127.0.0.1:8000')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ launches: data })
+    })
+    .catch(console.log) 
+  }
+  render() {
+    return (
+      <Launches launches={this.state.launches} />
+    )
+  }
+}
+
+export default App;
---
+++ frontend/src/components/launches.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Launches = ({ launches }) => {
+    return (
+      <div>
+        <center>
+        <h1>SpaceX Launches</h1>
+        {launches.map((launch) => (
+          <div >
+            <div>
+              <h5>{launch.mission_name}</h5>
+              <h5>{launch.launch_year}</h5>
+            </div>
+          </div>
+        ))}
+        </center>
+      </div>
+    )
+  };
+
+  export default Launches
