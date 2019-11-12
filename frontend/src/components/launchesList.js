ded9194355e5f80f1a5c89e99f47b2d2911c188f
patrickvrb <patrickvrb@gmail.com>
2019-11-12 17:39:50 -0300

    components display in three tables

--- frontend/src/components/launchesList.js
+++ frontend/src/components/launchesList.js
@@ -4,23 +4,34 @@ const LaunchesList = ({ launchesList }) => {
   return (
     <div>
       <center>
-        <table>
+        <h2>List of five upcoming launches</h2>
+        <table cellPadding="10" >
           <tbody>
-          <tr>
-            <th>Mission name</th>
-            <th>Launch year</th>
-          </tr>
-          <tr>
-            {launchesList.map((launchesList) => (
-             launchesList.map((launch, id) => (
-                <div key={id}>
-                  <td>{launch.mission_name}</td>
-                  <td>{launch.launch_year}</td>
-                </div>
-              )) 
-            ))
-            }
-          </tr>
+            <tr>
+              <th>Mission name</th>
+              <th>Launch year</th>
+            </tr>
+            {launchesList[0].map((launch, id) => (
+              <tr key={id}>
+                <td>{launch.mission_name}</td>
+                <td>{launch.launch_year}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <h2>List of five past launches</h2>
+        <table cellPadding="10" >
+          <tbody>
+            <tr>
+              <th>Mission name</th>
+              <th>Launch year</th>
+            </tr>
+            {launchesList[1].map((launch, id) => (
+              <tr key={id}>
+                <td>{launch.mission_name}</td>
+                <td>{launch.launch_year}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </center>
--- frontend/src/components/launchesObjects.js
+++ frontend/src/components/launchesObjects.js
@@ -1,18 +1,28 @@
 import React from 'react'
 
 const LaunchesObjects = ({ launchesObjects }) => {
-  console.log("Objects: ", launchesObjects)
+  //console.log("Objects: ", launchesObjects)
   return (
     <center>
       <h1>SpaceX Launches</h1>
-      <table>
-        <tr>
-          <th>Mission name</th>
-          <th>Launch year</th>
-        </tr>
+      <table cellPadding="10">
+        <tbody>
           <tr>
-            {launchesObjects[0].mission_name}
+            <th></th>
+            <th><strong>Mission name</strong></th>
+            <th><strong>Launch year</strong></th>
           </tr>
+          <tr>
+            <td>Latest Launch</td>
+            <td>{launchesObjects[1].mission_name}</td>
+            <td>{launchesObjects[1].launch_year}</td>
+          </tr>
+          <tr>
+            <td>Next Launch</td>
+            <td>{launchesObjects[0].mission_name}</td>
+            <td>{launchesObjects[0].launch_year}</td>
+          </tr>
+        </tbody>
       </table>
     </center>
   )
